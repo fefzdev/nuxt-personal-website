02a2c7b6ab10f724025385ebbaaa2a3a2955feac
Félix Bouveret <bouveret.felix@gmail.com>
2020-02-21 20:57:34 +0100

    Add presentation component

---
+++ components/Sections/Presentation.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="presentation wht_bgr">
+    <div class="inner ">
+      <h2>{{ $t('presentSection.title') }}</h2>
+      <div class="fadeIn">
+        <p class="section_desc">
+          {{ $t('presentSection.description') }}
+        </p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.presentation {
+  .section_desc {
+    margin-bottom: 40px;
+
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+}
+</style>
