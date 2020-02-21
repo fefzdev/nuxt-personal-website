fb777b7dfde36ed4fb50e55afcec4b8d193b4548
Félix Bouveret <bouveret.felix@gmail.com>
2020-02-21 21:00:54 +0100

    Review hero component

--- components/Sections/Hero.vue
+++ components/Sections/Hero.vue
@@ -1,16 +1,16 @@
 <template>
   <section class="hero">
     <div class="inner--hero">
-      <p>
+      <p lang="en">
         {{ $t('heroSection.hello') }}
       </p>
-      <h1 class="hero__title">
+      <h1 class="title">
         {{ $t('heroSection.title') }}
       </h1>
-      <p class="hero__desc">
+      <p class="desc">
         {{ $t('heroSection.description') }}
       </p>
-      <p>
+      <p lang="en">
         {{ $t('heroSection.intro') }}
       </p>
     </div>
@@ -24,4 +24,46 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.hero {
+  position: relative;
+  display: flex;
+  width: 100%;
+  height: 100vh;
+  min-height: 600px;
+  padding-top: 80px;
+
+  @media only screen and (min-width: 1px) and (max-width: 900px) {
+    padding-top: 50px;
+  }
+
+  .inner--hero {
+    max-width: 100vw;
+    width: 100%;
+    text-align: center;
+    position: relative;
+    margin: auto;
+
+    > * {
+      margin: 0 auto;
+    }
+
+    p {
+      font-size: 1.7em;
+      max-width: 700px;
+      width: 90%;
+      margin-bottom: 20px;
+
+      @media only screen and (min-width: 1px) and (max-width: 900px) {
+        font-size: 1.375em;
+      }
+    }
+
+    .title {
+      line-height: 1.1em;
+      margin-bottom: 40px;
+      max-width: 1000px;
+      width: 90%;
+    }
+  }
+}
 </style>
