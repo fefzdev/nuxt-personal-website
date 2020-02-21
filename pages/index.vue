8bb8716d4a908ac5d7cc5912ad7a83fa959e38d0
Félix Bouveret <bouveret.felix@gmail.com>
2020-02-21 21:01:52 +0100

    Review index page

--- pages/index.vue
+++ pages/index.vue
@@ -2,17 +2,32 @@
   <div>
     <hero-section />
     <stories-section />
+    <likes-section />
+    <uses-section />
+    <pres-section />
+    <capital-section />
+    <mamy />
   </div>
 </template>
 
 <script>
 import HeroSection from '~/components/Sections/Hero'
 import StoriesSection from '~/components/Sections/Stories'
+import LikesSection from '~/components/Sections/Likes'
+import UsesSection from '~/components/Sections/Uses'
+import PresSection from '~/components/Sections/Presentation'
+import CapitalSection from '~/components/Sections/Capital-gain'
+import Mamy from '~/components/Sections/Mamy'
 
 export default {
   components: {
     HeroSection,
-    StoriesSection
+    StoriesSection,
+    LikesSection,
+    UsesSection,
+    PresSection,
+    CapitalSection,
+    Mamy
   }
 }
 </script>
